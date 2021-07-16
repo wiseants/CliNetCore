43f7325bc273d0bd4168967cc7277dc973903eaf
hcan <hcan@trinitysoft.co.kr>
2021-07-16 16:37:50 +0900

    디크리즈 커맨드 추가.

---
+++ CliNetCore/Cores/Commands/DecreaseCommand.cs
@@ -0,0 +1,37 @@
+﻿using AustinHarris.JsonRpc;
+using CliNetCore.Interfaces;
+using CommandLine;
+using System;
+using System.Net;
+using System.Text;
+using Utility.Network;
+
+namespace CliNetCore.Cores.Commands
+{
+    [Verb("decrease", HelpText = "Decrease number")]
+    public class DecreaseCommand : IAction
+    {
+        public bool IsValid => true;
+
+        [Option('t', "target", Required = true, HelpText = "target number.")]
+        public int TargetNumber
+        {
+            get;
+            set;
+        }
+
+        public int Action()
+        {
+            IPEndPoint point = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8055);
+
+            JsonRpcClient client = new JsonRpcClient(point, Encoding.UTF8);
+            var result = client.Invoke<int>("Decr", new object[] { TargetNumber });
+            result.Subscribe((JsonResponse<int> response) =>
+            {
+                Console.WriteLine(response.Result);
+            });
+
+            return 1;
+        }
+    }
+}
